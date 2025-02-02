a00621308e5390945fc27b59a4351cc3f4b9e3d2
zharif.zubaidi <zharif96@gmail.com>
2025-02-02 10:20:24 +0800

    Section 4 challenge

---
+++ CppStudyVSCode/Sec4Challenge/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number = 0;
+    
+    cout << "Enter your favourite number between 1 and 100: " << endl;
+    cin >> number;
+    cout << "Amazing!! That's my favourite number too." << endl;
+    cout << "No really!! " << number << " is my favourite number!" << endl;
+
+    return 0;
+}
