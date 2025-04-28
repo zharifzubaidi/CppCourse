f8d2584c759779c5520548667710bf93f1d4b319
Zharif Zubaidi <zharif.aizuddin@stratusauto.com>
2025-04-28 11:05:25 +0800

    chore: change directory

---
+++ CppStudyVSCode/Section9CtrlProgFlow/IfStatements/main.cpp
@@ -0,0 +1,30 @@
+// Section 9
+// If Statement
+#include <iostream>
+#include <vector>
+
+// Use selected function from std namespace
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector; // Must include this line to use vector
+
+int main(){
+    //Variable declaration
+    int num {};
+    const int min {10};
+    const int max {100};
+
+    // Input
+    cout << "Enter a number between " << min << " and " << max << ";";
+    cin >> num;
+
+    // Processing
+    if (num >= min)
+        cout << num << "is greater than " << min << endl;
+
+    // Output
+
+
+    return 0;
+}
